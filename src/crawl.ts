00568aae26c67fcf0369c1cdaca156f19415f485
Helmi Satria <satriahelmi@gmail.com>
2023-05-07 22:58:21 +0700

    fix: i2 issue. tweets not found on live tab when using date parameters

--- src/crawl.ts
+++ src/crawl.ts
@@ -38,6 +38,10 @@ const filteredFields = [
   "media_type",
 ];
 
+type StartCrawlTwitterParams = {
+  twitterSearchUrl?: string;
+};
+
 export async function crawl({
   ACCESS_TOKEN,
   SEARCH_KEYWORDS,
@@ -61,6 +65,8 @@ export async function crawl({
   const FILE_NAME =
     `${FOLDER_DESTINATION}/${SEARCH_KEYWORDS} ${NOW}.csv`.replace(/ /g, "_");
   console.info(chalk.blue("\nOpening twitter search page...\n"));
+
+  let TWEETS_NOT_FOUND_ON_LIVE_TAB = false;
 
   const browser = await chromium.launch({
     headless: false,
@@ -101,186 +107,219 @@ export async function crawl({
     route.continue();
   });
 
-  // await page.goto("https://twitter.com/search-advanced");
-  await page.goto("https://twitter.com/search-advanced?f=live", {
-    waitUntil: "networkidle",
-  });
+  async function startCrawlTwitter({
+    twitterSearchUrl = "https://twitter.com/search-advanced?f=live",
+  }: StartCrawlTwitterParams = {}) {
+    await page.goto(twitterSearchUrl, {
+      waitUntil: "networkidle",
+    });
 
-  // check is current page url is twitter login page (have /login in the url)
-  const isLoggedIn = !page.url().includes("/login");
+    // check is current page url is twitter login page (have /login in the url)
+    const isLoggedIn = !page.url().includes("/login");
 
-  if (!isLoggedIn) {
-    console.error("Invalid twitter auth token. Please check your auth token");
-    return browser.close();
-  }
+    if (!isLoggedIn) {
+      console.error("Invalid twitter auth token. Please check your auth token");
+      return browser.close();
+    }
 
-  // wait until allOfTheseWords input is visible
-  await page.waitForSelector('input[name="allOfTheseWords"]');
+    // wait until allOfTheseWords input is visible
+    await page.waitForSelector('input[name="allOfTheseWords"]');
 
-  await page.click('input[name="allOfTheseWords"]');
+    await page.click('input[name="allOfTheseWords"]');
 
-  console.info(`Filling in keywords: ${SEARCH_KEYWORDS}`);
-  await page.fill('input[name="allOfTheseWords"]', SEARCH_KEYWORDS);
+    console.info(`Filling in keywords: ${SEARCH_KEYWORDS}`);
+    await page.fill('input[name="allOfTheseWords"]', SEARCH_KEYWORDS);
 
-  if (SEARCH_FROM_DATE) {
-    await page.click('div[aria-label="From"]');
-    const selects = await page.$$('div[aria-label="From"] select');
-    const monthSelect = selects[0];
-    const daySelect = selects[1];
-    const yearSelect = selects[2];
+    if (SEARCH_FROM_DATE) {
+      await page.click('div[aria-label="From"]');
+      const selects = await page.$$('div[aria-label="From"] select');
+      const monthSelect = selects[0];
+      const daySelect = selects[1];
+      const yearSelect = selects[2];
 
-    // expect value to be in this format: 2023-01-01 00:00:00
-    const [day, month, year] = SEARCH_FROM_DATE.split(" ")[0].split("-");
-    const monthName = dayjs()
-      .month(parseInt(month) - 1)
-      .format("MMMM");
+      // expect value to be in this format: 2023-01-01 00:00:00
+      const [day, month, year] = SEARCH_FROM_DATE.split(" ")[0].split("-");
+      const monthName = dayjs()
+        .month(parseInt(month) - 1)
+        .format("MMMM");
 
-    await monthSelect.selectOption(monthName);
-    await daySelect.selectOption(Number(day).toString());
-    await yearSelect.selectOption(Number(year).toString());
-  }
+      await monthSelect.selectOption(monthName);
+      await daySelect.selectOption(Number(day).toString());
+      await yearSelect.selectOption(Number(year).toString());
+    }
 
-  if (SEARCH_TO_DATE) {
-    await page.click('div[aria-label="To"]');
-    const selectsTo = await page.$$('div[aria-label="To"] select');
+    if (SEARCH_TO_DATE) {
+      await page.click('div[aria-label="To"]');
+      const selectsTo = await page.$$('div[aria-label="To"] select');
 
-    const monthSelectTo = selectsTo[0];
-    const daySelectTo = selectsTo[1];
-    const yearSelectTo = selectsTo[2];
+      const monthSelectTo = selectsTo[0];
+      const daySelectTo = selectsTo[1];
+      const yearSelectTo = selectsTo[2];
 
-    // expect value to be in this format: 2023-01-01 00:00:00
-    const [day, month, year] = SEARCH_TO_DATE.split(" ")[0].split("-");
-    // month is still number, not string. convert it first to month name
-    const monthName = dayjs()
-      .month(parseInt(month) - 1)
-      .format("MMMM");
+      // expect value to be in this format: 2023-01-01 00:00:00
+      const [day, month, year] = SEARCH_TO_DATE.split(" ")[0].split("-");
+      // month is still number, not string. convert it first to month name
+      const monthName = dayjs()
+        .month(parseInt(month) - 1)
+        .format("MMMM");
 
-    await monthSelectTo.selectOption(monthName);
-    await daySelectTo.selectOption(Number(day).toString());
-    await yearSelectTo.selectOption(Number(year).toString());
-  }
+      await monthSelectTo.selectOption(monthName);
+      await daySelectTo.selectOption(Number(day).toString());
+      await yearSelectTo.selectOption(Number(year).toString());
+    }
 
-  // Press Enter
-  await page.press('input[name="allOfTheseWords"]', "Enter");
+    // Press Enter
+    await page.press('input[name="allOfTheseWords"]', "Enter");
 
-  let timeoutCount = 0;
-  let lastScrollId;
-  let lastTweetCreatedAt;
-  let additionalTweetsCount = 0;
+    let timeoutCount = 0;
+    let lastScrollId;
+    let lastTweetCreatedAt;
+    let additionalTweetsCount = 0;
 
-  const allData = {
-    tweets: [],
-    users: [],
-  };
+    const allData = {
+      tweets: [],
+      users: [],
+    };
 
-  async function scrollAndSave() {
-    timeoutCount = 0;
+    async function scrollAndSave() {
+      timeoutCount = 0;
 
-    while (allData.tweets.length < TARGET_TWEET_COUNT) {
-      // Wait for the next response or 10 seconds, whichever comes first
-      const response = await Promise.race([
-        page.waitForResponse((response) =>
-          response.url().includes("adaptive.json")
-        ),
-        page.waitForTimeout(3000),
-      ]);
+      while (allData.tweets.length < TARGET_TWEET_COUNT) {
+        // Wait for the next response or 10 seconds, whichever comes first
+        const response = await Promise.race([
+          page.waitForResponse((response) =>
+            response.url().includes("adaptive.json")
+          ),
+          page.waitForTimeout(3000),
+        ]);
 
-      if (response) {
-        const responseBody = await response.json();
+        if (response) {
+          const responseBody = await response.json();
 
-        const tweets = responseBody.globalObjects.tweets;
-        const users = responseBody.globalObjects.users;
+          const tweets = responseBody.globalObjects.tweets;
+          const users = responseBody.globalObjects.users;
 
-        if (!Object.keys(tweets).length) {
-          // found text "not found" on the page
-          if (await page.getByText("No results for").count()) {
-            console.info("No tweets found for the search criteria");
-            break;
+          if (!Object.keys(tweets).length) {
+            // found text "not found" on the page
+            if (await page.getByText("No results for").count()) {
+              TWEETS_NOT_FOUND_ON_LIVE_TAB = true;
+              console.info("No tweets found for the search criteria");
+              break;
+            }
           }
-        }
 
-        // console.info("tweet posted at -->", tweetCreatedAt);
+          // console.info("tweet posted at -->", tweetCreatedAt);
 
-        lastScrollId =
-          responseBody?.timeline?.instructions?.[1]?.replaceEntry?.entry
-            ?.content?.operation?.cursor?.value;
+          lastScrollId =
+            responseBody?.timeline?.instructions?.[1]?.replaceEntry?.entry
+              ?.content?.operation?.cursor?.value;
 
-        const headerRow = filteredFields.join(";") + "\n";
+          const headerRow = filteredFields.join(";") + "\n";
 
-        if (allData.tweets.length === 0) {
-          appendCsv(FILE_NAME, headerRow);
-        }
+          if (allData.tweets.length === 0) {
+            appendCsv(FILE_NAME, headerRow);
+          }
 
-        // add tweets and users to allData
-        allData.tweets.push(...Object.values(tweets));
-        allData.users.push(...Object.values(users));
+          // add tweets and users to allData
+          allData.tweets.push(...Object.values(tweets));
+          allData.users.push(...Object.values(users));
 
-        // write tweets to CSV file
-        const comingTweets = Object.values(tweets);
+          // write tweets to CSV file
+          const comingTweets = Object.values(tweets);
 
-        if (!fs.existsSync(FOLDER_DESTINATION)) {
-          const dir = fs.mkdirSync(FOLDER_DESTINATION, { recursive: true });
-          const dirFullPath = path.resolve(dir);
+          if (!fs.existsSync(FOLDER_DESTINATION)) {
+            const dir = fs.mkdirSync(FOLDER_DESTINATION, { recursive: true });
+            const dirFullPath = path.resolve(dir);
 
-          console.info(chalk.green(`Created new directory: ${dirFullPath}`));
-        }
+            console.info(chalk.green(`Created new directory: ${dirFullPath}`));
+          }
 
-        const rows = comingTweets.reduce((prev: [], current: any) => {
-          const tweet = pick(current, filteredFields);
-          tweet["full_text"] = `"${tweet["full_text"].replace(/\n/g, "\\n")}"`;
-          tweet["media_url_https"] =
-            `"${current?.entities?.media?.[0]?.media_url_https ?? ""}"` || "";
-          tweet["media_type"] = `"${
-            current?.entities?.media?.[0]?.type ?? ""
-          }"`;
+          const rows = comingTweets.reduce((prev: [], current: any) => {
+            const tweet = pick(current, filteredFields);
+            tweet["full_text"] = `"${tweet["full_text"].replace(
+              /\n/g,
+              "\\n"
+            )}"`;
+            tweet["media_url_https"] =
+              `"${current?.entities?.media?.[0]?.media_url_https ?? ""}"` || "";
+            tweet["media_type"] = `"${
+              current?.entities?.media?.[0]?.type ?? ""
+            }"`;
 
-          const row = Object.values(tweet).join(";");
+            const row = Object.values(tweet).join(";");
 
-          return [...prev, row];
-        }, []);
+            return [...prev, row];
+          }, []);
 
-        const csv = (rows as []).join("\n") + "\n";
-        const fullPathFilename = appendCsv(FILE_NAME, csv);
+          const csv = (rows as []).join("\n") + "\n";
+          const fullPathFilename = appendCsv(FILE_NAME, csv);
 
-        console.info(chalk.blue(`Your tweets saved to: ${fullPathFilename}`));
+          console.info(chalk.blue(`Your tweets saved to: ${fullPathFilename}`));
 
-        // progress:
-        console.info(
-          chalk.yellow(`Total tweets saved: ${allData.tweets.length}`)
-        );
-        additionalTweetsCount += comingTweets.length;
-
-        // for every multiple of 100, wait for 5 seconds
-        if (additionalTweetsCount > 100) {
-          additionalTweetsCount = 0;
+          // progress:
           console.info(
-            chalk.gray("\n--Taking a break, waiting for 10 seconds...")
+            chalk.yellow(`Total tweets saved: ${allData.tweets.length}`)
           );
-          await page.waitForTimeout(10_000);
-        } else if (additionalTweetsCount > 20) {
-          // for every multiple of 20, wait for 3 seconds
-          // console.info("Taking a break, waiting for 3 seconds...");
-          await page.waitForTimeout(DELAY_EACH_TWEET_SECONDS * 1000);
-        }
-      } else {
-        timeoutCount++;
-        if (timeoutCount === 1) {
-          console.info(chalk.gray("No additional tweet, scrolling more..."));
-        } else {
-          console.info(
-            chalk.gray("Still no additional tweet, scrolling more...")
-          );
-        }
+          additionalTweetsCount += comingTweets.length;
 
-        const findLastTweet = async () => {
-          let lastTweet: ElementHandle<SVGElement | HTMLElement>;
-
-          while (!lastTweet) {
-            lastTweet = await page.$(
-              "article[data-testid='tweet']:last-child div[data-testid='tweetText'] span"
+          // for every multiple of 100, wait for 5 seconds
+          if (additionalTweetsCount > 100) {
+            additionalTweetsCount = 0;
+            console.info(
+              chalk.gray("\n--Taking a break, waiting for 10 seconds...")
             );
+            await page.waitForTimeout(10_000);
+          } else if (additionalTweetsCount > 20) {
+            // for every multiple of 20, wait for 3 seconds
+            // console.info("Taking a break, waiting for 3 seconds...");
+            await page.waitForTimeout(DELAY_EACH_TWEET_SECONDS * 1000);
+          }
+        } else {
+          timeoutCount++;
+          if (timeoutCount === 1) {
+            console.info(chalk.gray("No additional tweet, scrolling more..."));
+          } else {
+            console.info(
+              chalk.gray("Still no additional tweet, scrolling more...")
+            );
+          }
 
-            if (!lastTweet) {
+          const findLastTweet = async () => {
+            let lastTweet: ElementHandle<SVGElement | HTMLElement>;
+
+            while (!lastTweet) {
+              lastTweet = await page.$(
+                "article[data-testid='tweet']:last-child div[data-testid='tweetText'] span"
+              );
+
+              if (!lastTweet) {
+                await page.evaluate(() => {
+                  window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                  });
+                });
+
+                await page.waitForTimeout(1_000);
+              }
+
+              await page.evaluate(() => {
+                const lastTweet = document.querySelector(
+                  "article[data-testid='tweet']:last-child"
+                );
+
+                lastTweet?.scrollIntoView({ behavior: "smooth" });
+              });
+
+              await page.waitForTimeout(1_000);
+            }
+
+            return lastTweet;
+          };
+
+          const clickLastTweet = async () => {
+            const lastTweet = await findLastTweet();
+            await lastTweet.click({ timeout: 1_000 }).catch(async () => {
               await page.evaluate(() => {
                 window.scrollTo({
                   top: 0,
@@ -288,74 +327,64 @@ export async function crawl({
                 });
               });
 
-              await page.waitForTimeout(1_000);
-            }
-
-            await page.evaluate(() => {
-              const lastTweet = document.querySelector(
-                "article[data-testid='tweet']:last-child"
-              );
-
-              lastTweet?.scrollIntoView({ behavior: "smooth" });
+              await clickLastTweet();
             });
+          };
 
+          const lastTweet = await page.$(
+            "article[data-testid='tweet']:last-child div[data-testid='tweetText'] span"
+          );
+
+          if (!lastTweet) {
+            console.info(chalk.gray("Still looking for the tweets..."));
+            await findLastTweet();
             await page.waitForTimeout(1_000);
           }
 
-          return lastTweet;
-        };
+          await clickLastTweet();
 
-        const clickLastTweet = async () => {
-          const lastTweet = await findLastTweet();
-          await lastTweet.click({ timeout: 1_000 }).catch(async () => {
-            await page.evaluate(() => {
-              window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            });
+          await page.goBack();
+          await page.waitForURL("https://twitter.com/search**");
 
-            await clickLastTweet();
+          // scroll to the last tweet
+          await page.evaluate(() => {
+            const lastTweet = document.querySelector(
+              "article[data-testid='tweet']:last-child"
+            );
+
+            console.log("lastTweet", lastTweet?.textContent);
           });
-        };
 
-        const lastTweet = await page.$(
-          "article[data-testid='tweet']:last-child div[data-testid='tweetText'] span"
-        );
-
-        if (!lastTweet) {
-          console.info(chalk.gray("Still looking for the tweets..."));
-          await findLastTweet();
-          await page.waitForTimeout(1_000);
+          await scrollAndSave(); // call the function again to resume scrolling
+          break;
         }
 
-        await clickLastTweet();
-
-        await page.goBack();
-        await page.waitForURL("https://twitter.com/search**");
-
-        // scroll to the last tweet
-        await page.evaluate(() => {
-          const lastTweet = document.querySelector(
-            "article[data-testid='tweet']:last-child"
-          );
-
-          console.log("lastTweet", lastTweet?.textContent);
-        });
-
-        await scrollAndSave(); // call the function again to resume scrolling
-        break;
+        await page.evaluate(() =>
+          window.scrollTo(0, document.body.scrollHeight)
+        );
       }
+    }
 
-      await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+    await scrollAndSave();
+
+    if (allData.tweets.length) {
+      console.info(
+        `Already got ${allData.tweets.length} tweets, done scrolling...`
+      );
+    } else {
+      console.info("No tweets found for the search criteria");
     }
   }
 
-  await scrollAndSave();
+  await startCrawlTwitter();
 
-  console.info(
-    `Already got ${allData.tweets.length} tweets, done scrolling...`
-  );
+  if (TWEETS_NOT_FOUND_ON_LIVE_TAB && (SEARCH_FROM_DATE || SEARCH_TO_DATE)) {
+    console.info('No tweets found on "Latest" tab, trying "Top" tab...');
+
+    await startCrawlTwitter({
+      twitterSearchUrl: "https://twitter.com/search-advanced",
+    });
+  }
 
   if (!DEBUG_MODE) {
     await browser.close();
